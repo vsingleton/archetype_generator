a72e9a734d4d19b08bafa478e91eb555d41b15b2
vsingleton <vsingleton@gmail.com>
2016-10-28 05:48:22 -0400

    FACES-2941 using the gradle mavenCentral keyword as suggested by Andrea (no change to gen.pl needed)

--- sync.sh
+++ sync.sh
@@ -1,59 +1,59 @@
 
-# 5.x
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-5.x/archetype/icefaces-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-5.x/archetype/jsf-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-5.x/archetype/alloy-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-5.x/archetype/metal-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-5.x/archetype/primefaces-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-5.x/archetype/richfaces-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-
-# 4.x
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-4.x/archetype/icefaces-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-4.x/archetype/jsf-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-4.x/archetype/alloy-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-4.x/archetype/metal-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-4.x/archetype/primefaces-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-4.x/archetype/richfaces-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-
-# 3.x
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-3.x/archetype/icefaces-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-3.x/archetype/jsf-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-3.x/archetype/alloy-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-3.x/archetype/metal-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-3.x/archetype/primefaces-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-3.x/archetype/richfaces-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-
-# 2.x
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-2.x/archetype/icefaces-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-2.x/archetype/jsf-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-2.x/archetype/alloy-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-2.x/archetype/metal-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-2.x/archetype/primefaces-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
-dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-2.x/archetype/richfaces-portlet
-if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# # 5.x
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-5.x/archetype/icefaces-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-5.x/archetype/jsf-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-5.x/archetype/alloy-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-5.x/archetype/metal-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-5.x/archetype/primefaces-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-5.x/archetype/richfaces-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# 
+# # 4.x
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-4.x/archetype/icefaces-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-4.x/archetype/jsf-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-4.x/archetype/alloy-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-4.x/archetype/metal-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-4.x/archetype/primefaces-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-4.x/archetype/richfaces-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# 
+# # 3.x
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-3.x/archetype/icefaces-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-3.x/archetype/jsf-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-3.x/archetype/alloy-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-3.x/archetype/metal-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-3.x/archetype/primefaces-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-3.x/archetype/richfaces-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# 
+# # 2.x
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-2.x/archetype/icefaces-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-2.x/archetype/jsf-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-2.x/archetype/alloy-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-2.x/archetype/metal-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-2.x/archetype/primefaces-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
+# dir=~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-2.x/archetype/richfaces-portlet
+# if [ ! -d $dir ]; then mkdir -p $dir && echo made $dir ...; fi
 
 # 5.x
 rsync -vax deps/archetypes/icefaces/war/liferay/7.0.x/jsf-2.2/* ~/Projects/liferay.com/liferay-faces/liferay-faces-bridge-ext-5.x/archetype/icefaces-portlet/.
